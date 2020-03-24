3adb3df8778803d87a505b4be89c7839732539d4
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-03-24 20:11:50 +0200

    broadcasting events, sharing location, event ack

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,40 @@
 const socket = io()
 
-socket.on('countUpdated', (count)=>{
-  console.log('The count has been updated!', count)
-}) 
+// socket.on('countUpdated', (count)=>{
+//   console.log('The count has been updated!', count)
+// }) 
 
-document.querySelector('#increment').addEventListener('click', ()=>{
-  console.log('clicked')
-  socket.emit('increment')
+// document.querySelector('#increment').addEventListener('click', ()=>{
+//   console.log('clicked')
+//   socket.emit('increment')
+// })
+
+socket.on('message', (message)=>{
+  console.log(message)
+})
+
+document.querySelector('#submitButton').addEventListener('submit', (e)=>{
+  e.preventDefault()
+  const message = e.target.elements.message.value
+  socket.emit('sendMessage', message, (error)=>{
+    if(error){
+      return console.log(error)
+    }
+    console.log('The message was delivered!')
+  })
+})
+
+document.querySelector('#send-location').addEventListener('click', ()=>{
+  if(!navigator.geolocation){
+    return alert('Geolocation is not supported by your browser.')
+  }
+  navigator.geolocation.getCurrentPosition((position)=>{
+    socket.emit('sendLocation',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, ()=>{
+      console.log('location shared')
+    })
+  })
+  
 })
